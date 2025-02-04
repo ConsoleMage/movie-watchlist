4793ae190f5cce33b9bcdec984bd078516d6f2a4
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-02-04 18:42:05 +0800

    asdasfasdfasdf

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ async function getMovie() {
                     <img id="add-icon" />
                     <p id="watch">Watchlist</p>
                 </div>
-
+                <div id="result-3">
+                    <p id="summary">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
+                </div>
             </div>
         </div>
         <hr />
@@ -28,8 +30,6 @@ async function getMovie() {
 getMovie();
 
 
-// <div id="result-3">
-// <p id="summary">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
-// </div>
+
 
 
