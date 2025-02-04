07393a222ff6fb1dca69f04719360a0109d8df05
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-02-04 18:59:05 +0800

    regex things

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ async function getMovie() {
             <div id="result-info">
                 <div id="result-1">
                     <p>${data.Title} ⭐</p>
+                    <p>${data.Ratings[0].Value.split('/')[0]}</p>
                 </div>
                 <div id="result-2">
-                    <p id="runtime">116 min</p>
-                    <p id="genre">Drama, Mystery, Sci-fi</p>
+                    <p id="runtime">${data.Runtime}</p>
+                    <p id="genre">${data.Genre}</p>
                     <img id="add-icon" />
                     <p id="watch">Watchlist</p>
                 </div>
                 <div id="result-3">
-                    <p id="summary">A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</p>
+                    <p id="summary">${data.Plot}</p>
                 </div>
             </div>
         </div>
