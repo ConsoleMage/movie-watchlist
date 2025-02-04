7197ae6bf11cd16378456f5bc2b8d054675b08a0
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-02-04 15:51:05 +0800

    poster test

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ async function getMovie() {
     const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=145209d1");
     const data = await response.json();
     console.log(data);
+    let html = "";
+    document.getElementById("search-results").innerHTML = `
+            <div id="result">
+                <img src=${data.Poster} />
+                <h2>Home</h2>
+                <p>This is the home section.</p>
+            </div>
+    `;
+    
 }
 
 getMovie();
+
+
