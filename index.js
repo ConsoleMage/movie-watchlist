e91150e4998192b80b47f3e146f40e26ecf5f32e
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-02-05 17:33:11 +0800

    two functions

--- index.js
+++ index.js
@@ -6,18 +6,61 @@ const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 
 
-searchBtn.addEventListener("click", getMovie);
+searchBtn.addEventListener("click", getMovieByTitle);
 
 
 document.getElementById("search-bar").addEventListener("submit", function(event) {
     console.log("prevent default works here!!");
     event.preventDefault();
 });
-
-async function getMovie() {
+// By Title
+async function getMovieByTitle() {
     console.log(searchInput.value);
-    // By Search
-    // By Title
+
+
+    const response = await fetch(`https://www.omdbapi.com/?apikey=145209d1&t=${searchInput.value}`);
+    const data = await response.json();
+    console.log(data);
+    let movieObj = {
+        poster: data.Poster,
+        title: data.Title,
+        rating: data.Ratings[0].Value.split('/')[0],
+        runtime: data.Runtime,
+        genre: data.Genre,
+        plot: data.Plot
+    };
+    if (movieObj) {
+        document.getElementById("toggle-empty").style.display = "none";
+        document.getElementById("search-results").innerHTML += `
+            <div id="result">
+                <img src="${movieObj.poster}" />
+                <div id="result-info">
+                    <div id="result-1">
+                        <p>${movieObj.title} ⭐</p>
+                        <p>${movieObj.rating}</p>
+                    </div>
+                    <div id="result-2">
+                        <p id="runtime">${movieObj.runtime}</p>
+                        <p id="genre">${movieObj.genre}</p>
+                        <div id="add-icon"></div>
+                        <p id="watch">Watchlist</p>
+                    </div>
+                    <div id="result-3">
+                        <p id="summary">${movieObj.plot}</p>
+                    </div>
+                </div>
+            </div>
+            <hr />
+    `;
+    } else {
+        document.getElementById("search-results").innerHTML += `
+            <p>Unable to find what you’re looking for. Please try another search.</p>
+    `;
+    }
+}
+// By Search
+async function getMovieBySearch() {
+    console.log(searchInput.value);
     const response = await fetch(`https://www.omdbapi.com/?apikey=145209d1&t=${searchInput.value}`);
     const data = await response.json();
     console.log(data);
